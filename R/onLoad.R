551455a1250502a4ba79e4d5c2985034dd02cff0
Sasha Goodman <goodmansasha@gmail.com>
2018-01-18 20:29:21 -0800

    removed onLoad.R check of java which was causing format bug in devtools::install_github. Updated documentation.

--- R/onLoad.R
+++
@@ -1,4 +0,0 @@
-.onLoad <- function(libname, pkgname) {
- cat('If you see Java version 8 or higher, you are good to go. If not, see the documentation. \n')
- tmp= sys::exec_wait('java','-version')
-}
