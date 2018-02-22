43c21796dde6261fb5785bb4e7b7f739db0f13c9
Sasha Goodman <goodmansasha@gmail.com>
2018-02-22 12:37:35 -0800

    2 devtools::check() NOTEs fixed. .pdf document is removed and .onLoad() uses 'warning' instead of 'packageStartupMessage'

--- R/zzz.R
+++ R/zzz.R
@@ -4,12 +4,12 @@
                          ,stdout=TRUE)
                         , error = function(e) return(NA))
   if(any(is.na(char))){
-    packageStartupMessage("Could not access java from command line!
+    warning("Could not access java from command line!
                           Type ?tika for configuration tips. ")
   }
   java_installed = grepl('version "1\\.[789]',char) | grepl('version "[789]', char)
   if(!any(java_installed)){
-    packageStartupMessage('Please check that you have at least Java 7 or OpenJDK 1.7. 
+    warning('Please check that you have at least Java 7 or OpenJDK 1.7. 
                           Type ?tika for configuration tips.\n')
   }
 }
